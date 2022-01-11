68643437e22126cb08bc0ac654cdd838608acb50
VinicinBraga <vinicius@bragamatta.com.br>
2022-01-11 17:28:59 -0300

    Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api/index";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
